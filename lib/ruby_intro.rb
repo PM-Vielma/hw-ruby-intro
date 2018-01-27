2502091e9196a85393d5220cfc2f9806989a344b
PM-Vielma <pablom.vielma1130@gmail.com>
2018-01-26 18:36:48 -0600

    first two parts done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,101 @@
 # Part 1
 
 def sum arr
+	i = 0
+	arr.each {|a| i += a }
+	i
   # YOUR CODE HERE
 end
 
 def max_2_sum arr
+	i = 0
+	if arr.length() == 0
+		i = 0
+	elsif arr.length() == 1
+		i = arr[0]		
+	else
+		i1 = 0
+		i2 = 0
+
+=begin
+		if arr[0] > arr[1]
+			i1 = arr[0]
+			i2 = arr[1]
+		else
+			i1 = arr[1]
+			i2 = arr[0]
+	end
+=end
+		i1 = -4611686018427387902
+		i2 = -4611686018427387902
+		arr.each do |a|
+			if i1 < a
+				i2 = i1
+				i1 = a
+			elsif i2 < a
+				i2 = a
+			end
+		end
+		i = i1 + i2
+	end
+	i
+
   # YOUR CODE HERE
 end
 
 def sum_to_n? arr, n
+	truth = false
+	a = 1
+	for i in arr[0..-1]
+		for j in arr[a..-1]
+			if n == i + j
+				truth = true
+			end
+		end
+		a += 1
+	end
+	truth
   # YOUR CODE HERE
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	truth = true
+	vowels = ['a','A','e','E','i','I','o','O','u','U']
+	if s.length() == 0
+		truth = false
+	elsif (s[0] =~ /[[:alpha:]]/) == nil
+		truth = false
+	else 
+		vowels.each do |v|
+			if v == s[0]
+				truth = false
+			end
+		end
+	end
+	truth
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	truth = true
+	if s.length < 1
+		truth = false
+	elsif s.length == 1 && s != "0"
+		truth = false
+	elsif s[(s.length()-2)] != '0' || s[(s.length()-1)] != '0'
+		truth = false
+	else	s.each_char do |l|
+			if l != '0' && l != '1'
+				truth = false
+			end
+		end
+	end
+	truth
 end
 
 # Part 3
